4a21d27f8dbed341d9deba943d6ea702a3a5ea48
Atyawat Binreem <604234060@parichat.skru.ac.th>
2019-03-08 15:53:54 +0700

    last

--- src/providers/book-rest/book-rest.ts
+++ src/providers/book-rest/book-rest.ts
@@ -6,7 +6,7 @@ import { Book } from '../../../models/book.model';
 @Injectable()
 export class BookRestProvider {
   
-  private url="http://localhost:3000";
+  private url="http://armorz.herokuapp.com";
  
      
   constructor(public http: HttpClient) {    
